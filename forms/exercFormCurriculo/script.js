0d29994a8dd6b65c12bfe563dcaf31c85039d265
Maria Clara <claramedeiroswon@gmail.com>
2022-01-08 10:13:36 -0300

    ultimo requisito da questao 1

---
+++ forms/exercFormCurriculo/script.js
@@ -0,0 +1,13 @@
+function createStates(){
+    let stateOptions = ['AC', 'AL', 'AM', 'AP', 'BA', 'CE', 'DF', 'ES', 'GO', 'MA', 'MG', 'MS', 'MT', 'PA', 'PB', 'PE', 'PI', 'PR', 'RJ', 'RN', 'RO', 'RR', 'RS', 'SC', 'SE', 'SP', 'TO'];
+    let state = document.getElementById('state');
+
+    for(let i = 0; i < stateOptions.length; i+=1){
+        let option = document.createElement('option');
+        option.innerText = stateOptions[i];
+        option.setAttribute('value', stateOptions[i])
+        state.appendChild(option);
+    }
+}
+
+createStates();
